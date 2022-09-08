343cdc416a6542f11e98acf294720bad49ca2be8
Joe Kane <jkane@fsjd.org>
2022-09-08 19:12:56 +0200

    updates entrypoint;
    user must specify hapi or vue so we only launch one...
    ...this will go away, as vue will cease to be a separate server/service
    at some point...

--- beacon-entrypoint.sh
+++ beacon-entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 ### Super Flaky Startup Script For Testing :D
-
 cd /opt/beacon
-( cd srvr/hapi && yarn serve ) &
-cd uix/vue && yarn serve
 
+if [[ -n "${1}" ]]
+then
+  case "${1}" in
+    srvr|hapi) cd srvr/hapi;;
+      uix|vue) cd uix/vue;;
+  esac
+  yarn serve 
+
+else 
+  echo "Must Specifiy: hapi or vue" >&2
+  exit 2
+fi
