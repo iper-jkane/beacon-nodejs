a4fe42d391db51dbb3f8708c4aa9c50f20d83276
Joe Kane <jkane@fsjd.org>
2022-09-08 19:26:07 +0200

    updates hapi server; listens using tls

--- srvr/hapi/index.js
+++ srvr/hapi/index.js
@@ -1,11 +1,21 @@
 import Hapi from '@hapi/hapi';
 // import Boom from '@hapi/boom';
 import process from 'node:process'
+import Path from 'path'
+import fs from 'fs'
 import { BeaconRouter } from './router/beacon/plugin/BeaconRouter.js'
+
+import { fileURLToPath } from 'url';
+const __dirname = Path.dirname(fileURLToPath(import.meta.url));
 
 const hbsrv= new Hapi.Server({
   port: 9001,
-  host: 'localhost',
+  // host: 'localhost',
+  host: '0.0.0.0',
+  tls: {
+    key:  fs.readFileSync(__dirname + '/tls/server-key.pem'),
+    cert: fs.readFileSync(__dirname + '/tls/server-cert.pem'),
+  },
   routes: {
     cors: true,
   },
