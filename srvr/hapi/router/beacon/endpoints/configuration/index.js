7937226acab56aabee8df75f0f5ecc1e174aed70
Joe Kane <jkane@fsjd.org>
2022-09-15 16:49:26 +0200

    updates hapi endpoint; beaconConfigurationResponseRoute
    looks a lot like beaconInfoResponseRoute

--- srvr/hapi/router/beacon/endpoints/configuration/index.js
+++ srvr/hapi/router/beacon/endpoints/configuration/index.js
@@ -1,17 +1,36 @@
 import mongoose from 'mongoose'
 import { beaconConfigurationResponseSchema } from '../../../../../../schema/mongoose/beacon/framework/responses/beaconConfigurationResponse.js'
 
-const getBeaconConfiguration = async function(x){
+const getBeaconConfiguration = async function(req){
 	
-  const z = new beaconConfigurationResponseModel()
-  console.log( z )
-	return '<pre>{ "beaconConfigurationResponseModel": ' + z + '}</pre>'
+  // use existing mongoose / mongodb connection
+  const mdb = req.server.plugins.BeaconRouter.mdb
+
+  // use existing model; or create one if not found in this connection 
+  var beaconConfigurationResponseModel = mdb.models['beaconConfigurationResponseModel']
+  if ( ! beaconConfigurationResponseModel ){
+    // possible bug in Schema; have to pass in 'beaconConfigurationResponseSchema.options.collection'
+    console.log("NOTFOUND")
+    beaconConfigurationResponseModel = mdb.model('beaconConfigurationResponseModel', beaconConfigurationResponseSchema, beaconConfigurationResponseSchema.options.collection) 
+  }
   
+  var configDoc = await beaconConfigurationResponseModel.findOne({}) //, { _id: false })
+  if( !configDoc ){
+    // get from file?
+    configDoc = new beaconConfigurationResponseModel()
+    configDoc.save() // .then( doc => { return doc } )
+  }
+    // configDoc = await beaconConfigurationResponseModel.findOne({})
+  return JSON.stringify( configDoc )
 }
 
 const beaconConfigurationResponseRouteHandler = async function( req, res ){
-	// const bir = await getBeaconConfiguration()
-	return res.response( "<pre>" + JSON.stringify(beaconConfigurationResponseSchema.paths,null,2) + "</pre>" )
+  if(req.method == "get"){
+    return res.response( await getBeaconConfiguration(req) )
+  }else{
+    return {}
+    //Boom.methodNotAllowed('Cannot POST here!')
+  }
 }
 
 const beaconConfigurationResponseRoute = { 
