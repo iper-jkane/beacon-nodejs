f2f58c24bb9e9284ccb972b3d227d1ee9b33cfca
Joe Kane <jkane@fsjd.org>
2022-09-15 16:47:28 +0200

    update beaconConfigurationResponseSchema; correct structure
    as correct as far as I currently understand mongoose schemas ;)

--- schema/mongoose/beacon/framework/responses/beaconConfigurationResponse.js
+++ schema/mongoose/beacon/framework/responses/beaconConfigurationResponse.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
 
 import { beaconInformationalResponseMetaSchema } from './sections/beaconInformationalResponseMeta.js'
-import { beaconConfigurationSchema } from '../configuration/beaconConfigurationSchema.js'
+import { beaconConfigurationSchema }             from '../configuration/beaconConfigurationSchema.js'
 
-const beaconConfigurationResponseSchema = new mongoose.Schema()
-
-beaconConfigurationResponseSchema.add( beaconInformationalResponseMetaSchema )
-beaconConfigurationResponseSchema.add( beaconConfigurationSchema )
+const beaconConfigurationResponseSchema = new mongoose.Schema({
+  response: { type: beaconConfigurationSchema,       default: {} },
+      meta: { type: beaconInformationalResponseMetaSchema, default: {} },
+}, { collection: 'config.beaconConfigurationResponse' })
 
 export { beaconConfigurationResponseSchema }
