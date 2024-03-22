b724117e5d57e53c8f6ebb9d9fe7d297de4c762a
Joe Kane <jkane@fsjd.org>
2024-03-22 12:45:53 +0100

    first attempts at basic auth

--- srvr/hapi/router/beacon/plugin/BeaconRouter.js
+++ srvr/hapi/router/beacon/plugin/BeaconRouter.js
@@ -1,4 +1,8 @@
 import Boom from '@hapi/boom' 
+import * as Hoek from '@hapi/hoek'
+import { StatusCode } from 'status-code-enum'
+import hapiBasic from '@hapi/basic'
+import bcrypt from 'bcrypt'
 import glob from 'glob'
 import mongoose from 'mongoose'
 import HapiAutoRoute from 'hapi-auto-route'
@@ -7,7 +11,6 @@ import * as dotenv from 'dotenv'
 
 import { fileURLToPath } from 'url';
 const __dirname = Path.dirname(fileURLToPath(import.meta.url));
-
 
 import { rootRoute }                        from '../endpoints/root.js'
 import { beaconInfoResponseRoute }          from '../endpoints/info/index.js'
@@ -18,7 +21,16 @@ const defaultOptions = {
     method:  ['*'],
     path:    '/{path*}',
     handler: function( req, res ) {
-      return Boom.notAcceptable('You have offended this api server!');
+
+      // separate path?
+      if (req.path == "/favicon.ico"){ 
+        return res.response()
+        .type('image/x-icon')
+        .code(StatusCode.SuccessNoContent);
+      }
+  
+      return Boom.notAcceptable('You have offended this api server! ' + req.path);
+  
     }
   }
 }
@@ -32,7 +44,33 @@ const BeaconRouter = {
 
   // multiple: true,
   register: async function (server, options) {
-  
+ 
+    // register hapi basic auth plugin
+    await server.register(hapiBasic); 
+    
+    // basic auth structure for development; migrate to file, db, or turnkey solution 
+    // initially a simple user/pass combo + jwt / iana nomenclature: https://www.iana.org/assignments/jwt/jwt.xhtml
+    // this so it can provide authN + authZ data 
+    // to follow the beacon reference implementation would be to eventually align oauth scopes and beacon granularities
+    const authDb = {
+      users: [ { uid: 1000, given_name: "Biolo", family_name: "Gist", email: "null@dev.null", email_verified: true, user: "bgist", pass: " $2b$12$O9oo7dWbDgAPikRY8gAogeh7TRJ9ZctihsckEBKwVUexoGfjsAW1K" } ]
+    };
+
+    const validateCreds = async (req, user, pass, res) => {
+      console.log("validateCreds: " + req.url);
+      console.log("req.auth: " + JSON.stringify(req.auth, null, 2));
+      console.log("user: " + user);
+      console.log("pass: " + pass);
+      if ( Hoek.contain(authDb.users, { user: user } )){ 
+      return { isValid: true, credentials: { jwt: "Foo" } }
+      }
+      return { isValid: false }  
+    } 
+
+    server.auth.strategy('basic', 'basic', { validate: validateCreds });
+    // server.auth.default('basic');
+
+ 
     const mdbOptions = { connectTimeoutMS: 1000, serverSelectionTimeoutMS: 1000, socketTimeoutMS: 500 } 
 
     // using dotenv whilst open-sourcing this first attempt
@@ -55,11 +93,18 @@ const BeaconRouter = {
     server.route( beaconInfoResponseRoute )
     server.route( beaconConfigurationResponseRoute )
 
+    // authN
     server.route({
       method: ['post','get'],
-      path: '/auth/{path*}',
+      path: '/auth/{path}',
+      options: {
+        auth: 'basic'
+      },
       handler: function( req, res ) {
-        return res.response({ msg: 'Not Actually An Error, Boom!', statusCode: 201, madeUp: "stuff" }).code(201);
+        if ( req.path == "/auth/login" ){
+          return res.response("AuthN Success: just because it's you");
+        }
+        return res.response({ msg: 'Not Actually An Error, Boom!', statusCode: 201, madeUp: "stuff", path: req.path });
       }
     })
 
