513f8abab5896076c04fb14e1899e61f8cb71fb3
Joe Kane <jkane@fsjd.org>
2024-03-25 15:04:46 +0100

    authDb; adds clients array + reformats user object
    this is just for readability whilst testing

--- srvr/hapi/router/beacon/plugin/BeaconRouter.js
+++ srvr/hapi/router/beacon/plugin/BeaconRouter.js
@@ -53,7 +53,30 @@ const BeaconRouter = {
     // this so it can provide authN + authZ data 
     // to follow the beacon reference implementation would be to eventually align oauth scopes and beacon granularities
     const authDb = {
-      users: [ { uid: 1000, given_name: "Biolo", family_name: "Gist", email: "null@dev.null", email_verified: true, user: "bgist", pass: " $2b$12$O9oo7dWbDgAPikRY8gAogeh7TRJ9ZctihsckEBKwVUexoGfjsAW1K" } ]
+      clients: [ 
+                 { 
+                   client_id: 900, 
+                   client_name: "BioInst1", 
+                   client_secret: "c20fd1ae-8eb8-49d3-9a17-56c617546616", // uuid returned as jti (jwt id) 
+                   // remote address
+                   // X-Forwarded-For
+                   // X-Real-Ip
+                   ips_allowed: [ "10.10.10.1/32", "127.0.0.1/24", "10.128.0.0/24" ]                 }
+     ],
+
+      users: [
+               { 
+                 uid: 1000,
+                 gid: 1000,
+                 client_id: 900,
+                 given_name:     "Biolo",
+                 family_name:    "Gist",
+                 email:          "null@dev.null",
+                 email_verified: true,
+                 user:           "bgist",
+                 pass:           "$2b$12$O9oo7dWbDgAPikRY8gAogeh7TRJ9ZctihsckEBKwVUexoGfjsAW1K",
+               }
+             ]
     };
 
     const validateCreds = async (req, user, pass, res) => {
