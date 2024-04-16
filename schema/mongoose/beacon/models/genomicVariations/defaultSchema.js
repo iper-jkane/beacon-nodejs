54cd994c3225c0ac0eb5e659bde88d0396cad9c9
Joe Kane <jkane@fsjd.org>
2024-04-16 17:09:11 +0200

    updates beaconGenomicVariationsSchema (mongoose)

--- schema/mongoose/beacon/models/genomicVariations/defaultSchema.js
+++ schema/mongoose/beacon/models/genomicVariations/defaultSchema.js
@@ -12,6 +12,12 @@ const beaconGenomicVariationsSchema = mongoose.Schema({
       type: new mongoose.Schema({
         _id: false, // sub-doc
 
+        referenceBases: {
+          type: String,
+          validate: /^([ACGTUNRYSWKMBDHV\-\.]*)$/,
+          // required: true # not required by the spec
+        },
+
         alternateBases: {
           type: String,
           validate: /^([ACGTUNRYSWKMBDHV\-\.]*)$/,
@@ -21,8 +27,81 @@ const beaconGenomicVariationsSchema = mongoose.Schema({
         variantType: {
           type: String,
           required: true
-        }
+        },
 
+        location: {
+          type: new mongoose.Schema({
+            _id: false,
+
+            interval: {
+              type: new mongoose.Schema({
+
+                _id: false,
+
+                type: {
+                  type: String,
+                  default: "SequenceInterval",
+                  match: /^SequenceInterval$/, // enum might be quicker in principle
+                  immutable: true,
+                  required: true
+                },
+
+                start: {
+                  type: new mongoose.Schema({
+                    _id: false,
+                    type: {
+                      type: String,
+                      default: "Number",
+                      match: /^Number$/, // enum might be quicker in principle 
+                      required: true
+                    },
+                  
+                    value: {
+                      type: Number,
+                      required: true
+                    }
+                  }),
+                },
+
+                end: {
+                  type: new mongoose.Schema({
+                    _id: false,
+                    type: {
+                      type: String,
+                      default: "Number",
+                      match: /^Number$/, // enum might be quicker in principle 
+                      required: true
+                    },
+                  
+                    value: {
+                      type: Number,
+                      required: true
+                    }
+
+                  }),
+                }
+
+              }),
+              required: true
+            },
+
+            sequence_id: {
+              type: String,
+              validate: /^\w[^:]*:.+$/,
+              required: true
+            },
+
+            type: {
+              type: String,
+              default: "SequenceLocation",
+              match: /^SequenceLocation$/, // enum might be quicker in principle
+              immutable: true,
+              required: true
+            }
+
+          }),
+          required: true
+        }
       }),
       required: true
     }
