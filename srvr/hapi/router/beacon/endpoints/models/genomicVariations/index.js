dffb4b42595775abe9b7d0d8e7c35137fcf935dd
Joe Kane <jkane@fsjd.org>
2024-04-17 12:44:13 +0200

    updates request params payload; beaconGenomicVariationsParamsPayload
    name isn't great, can potentially refactor later

--- srvr/hapi/router/beacon/endpoints/models/genomicVariations/index.js
+++ srvr/hapi/router/beacon/endpoints/models/genomicVariations/index.js
@@ -5,7 +5,7 @@ import { beaconGenomicVariationsSchema } from '../../../../../../../schema/mongo
 
 const beaconGenomicVariationsParamsPayload = Joi.object({
 
-  alternateBases:             Joi.string(),
+  alternateBases:             Joi.string().pattern(/^([ACGTUNRYSWKMBDHV\-\.]*)$/),
   aminoacidChange:            Joi.string(),
   assemblyId:                 Joi.string(),
   end:                        Joi.array().items( Joi.number().integer().min(0) ).min(0).max(2), //could technically be Joi.ref['start'], but wouldn't be official spec?
@@ -14,14 +14,16 @@ const beaconGenomicVariationsParamsPayload = Joi.object({
   geneId:                     Joi.string(),
   genomicAlleleShortForm:     Joi.string(),
   includeResultsetResponses:  Joi.string().valid('ALL','HIT','MISS','NONE'), 
-  limit:                      Joi.number().integer().min(0).default(10),
-  referenceBases:             Joi.string(),
+  limit:                      Joi.number().integer().min(0).default(10).max(10).failover(10), // .max( beaconConfig.maxLimit )
+  mateName:                   Joi.string(),
+  referenceBases:             Joi.string().pattern(/^([ACGTUNRYSWKMBDHV\-\.]*)$/),
   referenceName:              Joi.string(),
   requestedSchema:            Joi.string(),
   skip:                       Joi.number().integer().min(0).default(0),
   start:                      Joi.array().items( Joi.number().integer().required().min(0) ).min(1).max(2).optional(),
   variantMaxLength:           Joi.number().integer().min(0),
-  variantMinLength:           Joi.number().integer().min(0)
+  variantMinLength:           Joi.number().integer().min(0),
+  variantType:                Joi.string()
 
 }) 
 
