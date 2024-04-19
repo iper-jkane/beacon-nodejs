693ba5222c680d4853ecd6edaa9946868f00b198
Joe Kane <jkane@fsjd.org>
2024-04-19 19:03:43 +0200

    allow for passing env vars to hydrated uix/vue client
    used in order
    VUE_APP_BNJS_PROTO -> BNJS_PROTO -> https
    VUE_APP_BNJS_HOST  -> BNJS_HOST  -> localhost
    VUE_APP_BNJS_PORT  -> BNJS_PORT  -> 8080

--- uix/vue/vue.config.js
+++ uix/vue/vue.config.js
@@ -1,4 +1,17 @@
-const beaconHost       = process.env.VUE_APP_BJNS_HOST ? process.env.VUE_APP_BJNS_HOST  : 'localhost'
+
+// there has to be a better way :D
+// presumably using webpack DefinePlugin to allow passing env vars not prefixed by 'VUE_APP_'
+if ( process.env.VUE_APP_BNJS_PROTO === undefined ) {
+  process.env.VUE_APP_BNJS_PROTO = process.env.BNJS_PROTO ? process.env.BNJS_PROTO : 'https'
+}
+if ( process.env.VUE_APP_BNJS_HOST === undefined ) {
+  process.env.VUE_APP_BNJS_HOST = process.env.BNJS_HOST ? process.env.BNJS_HOST : 'localhost'
+}
+if ( process.env.VUE_APP_BNJS_PORT === undefined ) {
+  process.env.VUE_APP_BNJS_PORT = process.env.BNJS_PORT ? process.env.BNJS_PORT : 8080
+}
+
+const beaconHost = process.env.VUE_APP_BNJS_HOST
 
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
