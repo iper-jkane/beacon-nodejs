8d153636cbd52171a49aebeeba6f9ae179b20634
Joe Kane <jkane@fsjd.org>
2024-04-21 11:54:28 +0200

    adds explicit webpack / vue config;

--- uix/vue/vue.config.js
+++ uix/vue/vue.config.js
@@ -34,4 +34,15 @@ module.exports = defineConfig({
       },
        allowedHosts: [ 'localhost', beaconUixHost ]
   },
+
+  chainWebpack: (config) => {
+    config.plugin('define').tap((definitions) => {
+      Object.assign(definitions[0], {
+        __VUE_OPTIONS_API__: 'false',
+        __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+      })
+      return definitions
+    })
+  }
 })
