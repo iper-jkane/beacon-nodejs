68cf7b9346c48f801fe251d45b73e54ffa590146
Joe Kane <jkane@fsjd.org>
2024-04-22 16:34:20 +0200

    update apiClient; create a wrapper around a wrapper
    actually; not sure about the axiosCaching idea, probs going to switch to
    pinia state management so will be able to use it's local cache instead
    
    in fact; we drop its use here.. pending removal

--- uix/vue/src/composables/api/apiClient.js
+++ uix/vue/src/composables/api/apiClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import axiosCache from 'axios-cache-plugin'
+import createAxiosCache from 'axios-cache-plugin'
 
 console.log( "VUE_APP_BNJS_UIX_URL: ", process.env.VUE_APP_BNJS_UIX_URL )
 console.log( "VUE_APP_BNJS_API_URL: ", process.env.VUE_APP_BNJS_API_URL )
@@ -17,16 +17,58 @@ let axiosClient = axios.create({
   baseURL: `${beaconApiProto}://${beaconApiHost}:${beaconApiPort}`
 })
 
+let axiosCache = createAxiosCache(
+  axiosClient, {
+    maxCacheSize: 15,
+    ttl: 30000
+  }
+)
+
 // axiosClient.interceptors.request.use(request => {
 //   console.log('Starting Request', JSON.stringify(request, null, 2))
 //   return request
 // })
 
 // you exported a wrapper for a wrapper
-let apiClient = axiosCache(axiosClient, {
-  maxCacheSize: 15,
-  ttl: 30000
-})
+const apiClient = {
+  client: axiosClient,
+//  cache:  axiosCache,
+  // options: {},
+  fetch: async function ( url, data, opts ){
+
+    var authBasic = {}
+    // if( typeof opts === 'object' ){
+    //   console.log("opts: ", opts)
+      if ( opts.auth == 'basic' ) {
+        authBasic = {
+          username: sessionStorage.getItem('auth.username'),
+          password: sessionStorage.getItem('auth.password')
+        }
+      }
+
+      if ( opts.bearer !== undefined ) {
+        this.client.config.assign(
+          {
+            headers: { 'authorization': opts.bearer }
+          }
+        )
+      }
+    // }
+    return await this.client({
+      url: url,
+      auth: authBasic,
+      data: data
+    }).then( (resp) => {
+         console.log("axios.resp: ", resp )
+         return resp.data ?? { apiWeirdness: "see console" }
+    }).catch( (err) => { 
+        console.log("axios.err:", err )
+        if ( err.response ){
+          return err.response.data ?? { apiClientError: "'Bad' bad; not good." }
+        }
+    })
+  }
+}
 
 
 export { apiClient }
