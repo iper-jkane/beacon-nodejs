5f1374e8f9083bca905537aea5f5613f7c97c72e
Joe Kane <jkane@fsjd.org>
2024-04-22 14:10:24 +0200

    adds files; GenomicVariationsIndex + route.js

---
+++ uix/vue/src/router/beacon/endpoints/models/genomicVariations/GenomicVariationsIndex.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { defineProps, ref, provide, inject } from 'vue'
+  import { useRoute, useRouter } from 'vue-router'
+
+  const apiClient = inject('apiClient')
+
+  const gVars = ref({}) 
+  provide( 'genomicVariations', gVars )
+
+  
+</script>
+
+<style>
+
+</style>
+
+<template>
+<pre>
+  {{ apiClient }}
+  <!-- {{ useRoute() }} -->
+  {{ props }}
+</pre>
+</template>
