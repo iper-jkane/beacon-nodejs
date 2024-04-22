240888f8d0a66bf715eb280906dd96f3d7c72156
Joe Kane <jkane@fsjd.org>
2024-04-22 16:52:06 +0200

    update GenomicVariationsIndex component;
    fetches API/g_variants endpoint using apiClient
    allows user to specify the limit request parameter in the posted query
    must be authenticated with Basic HTTP Auth
    results are vue-reactive

--- uix/vue/src/router/beacon/endpoints/models/genomicVariations/GenomicVariationsIndex.vue
+++ uix/vue/src/router/beacon/endpoints/models/genomicVariations/GenomicVariationsIndex.vue
@@ -1,13 +1,26 @@
 <script setup>
-  import { defineProps, ref, provide, inject } from 'vue'
+
+  import { defineProps, ref, reactive, provide, inject } from 'vue'
   import { useRoute, useRouter } from 'vue-router'
 
   const apiClient = inject('apiClient')
 
-  const gVars = ref({}) 
+  const gVars = ref("Nothing Yet...") 
   provide( 'genomicVariations', gVars )
 
+  // import Joi ??
+  const query = reactive({ requestParameters: {} })
   
+  query.requestParameters.limit = 2
+  
+// fetch endpoint parameters from server rather than hardcoding enums and the like...
+// and / or use the mongoose "middleware"
+
+// fetch the g_variants on demand
+const fetchGVars = async function() { 
+  gVars.value = await apiClient.fetch('/g_variants', { query: query }, { auth: 'basic' })
+}
+
 </script>
 
 <style>
@@ -16,8 +29,9 @@
 
 <template>
 <pre>
-  {{ apiClient }}
-  <!-- {{ useRoute() }} -->
-  {{ props }}
+  <input v-model="query.requestParameters.limit" placeholder="limit..." /><br/>
+  <button type="button" @click="fetchGVars()">Fetch/Refresh</button>
+
+  {{ gVars }}
 </pre>
 </template>
