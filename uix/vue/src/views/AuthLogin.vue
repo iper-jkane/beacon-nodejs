3852907c6495752cfa658c3ee7fb610e330d5ead
Joe Kane <jkane@fsjd.org>
2024-04-23 12:33:58 +0200

    working auth with new apiClient wrapper using axios.interceptors
    but see comments earlier in the log

--- uix/vue/src/views/AuthLogin.vue
+++ uix/vue/src/views/AuthLogin.vue
@@ -10,16 +10,62 @@
   })
 
   const authResp = ref()
+  const authMsg = ref("AuthBox")
+  const hasAuth = ref(false)
 
   /* const requestAuth = () => {} */
-  const requestAuth = function(){
+  const requestAuth = async function(){
     console.log(authData)
 
     const authDataUnref = unref(authData)
     sessionStorage.setItem('auth.username', authDataUnref.username)
     sessionStorage.setItem('auth.password', authDataUnref.password)
 
-    return apiClient.fetch( '/auth/login', {}, { auth: 'basic' } )
+    // disable apiClient default response/error handling
+//    apiClient.client.interceptors.response.eject(apiClient.defaultInterceptor)
+    // set response/error handling for this endpoint....
+    const authInterceptor = apiClient.client.interceptors.response.use(
+      (resp) => {
+        console.log( "Mig-29 Fulcrum: ", resp )
+        if ( resp.status == 200 ){ 
+          if ( resp.authResponse ){
+            hasAuth.value = true
+            sessionStorage.setItem('auth.creds', JSON.stringify(resp.authResponse))
+            sessionStorage.setItem('auth.jwt', resp.authResponse.jwt)
+            // console.log(JSON.parse(sessionStorage.getItem('auth.creds')))
+            authMsg.value = resp.authResponse.msg
+            router.push({ name: 'GenomicVariationsRoute'})
+            return resp.authResponse
+          }
+        }
+      },        
+
+      (err) => { 
+        console.log("ShotDown: ", apiClient.parseErrorMsg(err) )
+        authMsg.value = apiClient.parseErrorMsg(err)
+        return Promise.resolve(apiClient.parseErrorMsg(err))
+      }
+    )
+    
+    authResp.value = await apiClient.fetch( '/auth/login', {}, { auth: 'basic' } )
+    apiClient.client.interceptors.response.eject(authInterceptor)
+
+
+// .then( 
+//       (resp) => { return "foo" } 
+//     ).catch( (err) => { return "nah"  } )
+
+// .catch( 
+//       (err)  => {
+
+//         if (err.name == "AxiosError" ) { return "CLIENT ERROR" }
+//         console.log("error: ", err)
+//         return err.response.data ?? err
+
+//       }
+//     )
+/* authResp.value = r.data ? "Login Successful" : "OtherWeirdness"; */
+/* sessionStorage.setItem( 'jwt', JSON.stringify(r.data) ) */
   }
 
 </script>
@@ -38,13 +84,15 @@
 </style>
 
 <template>
+<div v-if="!hasAuth">
 <div id="AuthLogin">
-  {{ authResp }}
+  {{ authMsg }}
   <form>
     <input v-model="authData.username" placeholder="username" /><br/>
     <input v-model="authData.password" placeholder="password" type="password"/>
     <button type="button" @click="requestAuth">Login</button>
   </form>
+</div>
 </div>
 </template>
 
