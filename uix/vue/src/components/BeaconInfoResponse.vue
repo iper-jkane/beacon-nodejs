2c1083e90e043255b2b7a470df5e99a5f56f30d6
Joe Kane <jkane@fsjd.org>
2024-04-23 12:29:59 +0200

    add .catch to apiClient call

--- uix/vue/src/components/BeaconInfoResponse.vue
+++ uix/vue/src/components/BeaconInfoResponse.vue
@@ -9,7 +9,7 @@
   const apiClient = inject('apiClient')
 
   const beaconInfoResponse = ref("Fetching The Info Now...(that's a smashing blouse, btw!)")
-  beaconInfoResponse.value  = await apiClient.fetch( '/info', {}, { auth: 'basic' } ) 
+  beaconInfoResponse.value  = await apiClient.fetch( '/info', {}, { auth: 'basic' } ).catch( (err) => { return err } )
 
 </script>
 
