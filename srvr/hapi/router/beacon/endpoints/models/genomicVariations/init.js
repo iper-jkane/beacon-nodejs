792217b5992c672549d42f3f3ba7ac524ff50429
Joe Kane <jkane@fsjd.org>
2024-04-25 16:25:32 +0200

    adds basic model build + indexing for GenomicVariations
    runs at plugin startup

---
+++ srvr/hapi/router/beacon/endpoints/models/genomicVariations/init.js
@@ -0,0 +1,41 @@
+import { beaconGenomicVariationsSchema } from '../../../../../../../schema/mongoose/beacon/models/genomicVariations/defaultSchema.js'
+
+// todo: make generic / factory
+const buildIndexes = async function ( model, opts = {} ) {
+    
+  // if( opts.forceRebuild ){  }
+
+  const beaconGenomicVariationsModel = model // tmp
+  // until we can workout why Schema.index(...) + Model.createIndexes() isn't working...for composite keys
+  await beaconGenomicVariationsModel.db.db.command({
+    // Index All The Things! -- I mean don't really, but they are here as options for now;
+    createIndexes: beaconGenomicVariationsSchema.options.collection, 
+    indexes: [ 
+      { name: 'variantInternalId',  key: { 'variantInternalId':              1 } },
+      { name: 'biosampleId',        key: { 'caseLevelData.biosampleId':      1 } },
+      { name: 'genomicHGVSId',      key: { 'identifiers.genomicHGVSId' :     1 } },
+      { name: 'sequenceId',         key: { 'variation.location.sequence_id': 1 } }
+    ] 
+  })
+
+}
+
+// todo: make generic / factory
+const initGenomicVariationsModel = async function( mdb, opts = {} ) {
+
+  // if( opts.forceRecreate ){  }
+
+  // exists?  
+  var beaconGenomicVariationsModel = mdb.models['beaconGenomicVariationsModel']
+  if ( beaconGenomicVariationsModel === undefined ){
+
+    // create model -- try/catch
+    beaconGenomicVariationsModel = await mdb.model('beaconGenomicVariationsModel', beaconGenomicVariationsSchema, beaconGenomicVariationsSchema.options.collection)
+    // build indexes
+    buildIndexes(beaconGenomicVariationsModel)
+
+  }
+
+} 
+
+export { initGenomicVariationsModel }
