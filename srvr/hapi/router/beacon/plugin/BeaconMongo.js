fd20b6f1afd245b4be1f760ccffda998259a0f1e
Joe Kane <jkane@fsjd.org>
2024-05-02 16:22:51 +0200

    adds BeaconMongo plugin; can be specified as a dependency to...
    ...guarantee mongoose connection object availabilty.  That's the object,
    not the connection ;)

---
+++ srvr/hapi/router/beacon/plugin/BeaconMongo.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+
+import * as dotenv from 'dotenv'
+
+import Path from 'path'
+import { fileURLToPath } from 'url';
+const __dirname = Path.dirname(fileURLToPath(import.meta.url));
+
+import { initGenomicVariationsModel } from '../endpoints/models/genomicVariations/init.js'
+
+const BeaconMongo = {
+
+  pkg: {
+    name: 'BeaconMongo',
+    version: '0.0.1'
+  },
+
+  register: async function (server, options) {
+ 
+    console.log("Registering: BeaconMongo")
+    const mdbOptions = { connectTimeoutMS: 10000, serverSelectionTimeoutMS: 1000, socketTimeoutMS: 500 }
+
+    // using dotenv whilst open-sourcing this first attempt
+    dotenv.config({ path: __dirname + "/.env" })
+
+    // probs switch to BNJS_MDB_URI
+    const mdbHost   = process.env.mdbHost
+    const mdbPort   = process.env.mdbPort
+    const mdbDbName = process.env.mdbDbName 
+
+    const mdbUser = process.env.mdbUser ?? ""
+    const mdbPass = process.env.mdbPass ? `:${process.env.mdbPass}@` : ""
+
+    const mdbCreds = `${mdbUser}${mdbPass}`
+
+    try {
+
+      // try and establish a db connection
+      const mdb = await mongoose.connect(`mongodb://${mdbCreds}${mdbHost}:${mdbPort}/${mdbDbName}?directConnection=true`, mdbOptions)
+
+      // build collections from models
+      initGenomicVariationsModel(mdb) 
+
+      //global database connection
+      await server.expose('mdb', mdb )
+
+    }catch(err){
+      console.log("ERROR_DB:", err)
+      throw("...aaaAAAARRRRGH")
+    }
+  }
+}
+
+export { BeaconMongo }
